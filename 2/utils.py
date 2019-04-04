1be5ea31f3ccd74a0e160368891e23e5677c2ab3
GeorgiyYarosh <georgiy.yarosh@upsilonit.com>
2019-04-04 11:59:06 +0300

    Added script with utils for reading notMNIST data

---
+++ 2/utils.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+from pathlib import Path
+from PIL import Image
+
+
+def flatten(a):
+    return a.reshape(a.shape[0], a.shape[1] * a.shape[2])
+
+
+def to_one_hot(a, n):
+    result = np.zeros(shape=(a.shape[0], n))
+    result[np.arange(len(a)), a] = 1
+    return result
+
+
+def load_images(path, n):
+    labels, x, y = [], [], []
+    for i, d in enumerate(Path(path).iterdir()):
+        labels.append(d.name)
+        print(f'Loading {str(d)} ', end='')
+        for j, f in zip(range(n), d.iterdir()):
+            try:
+                with Image.open(f) as img:
+                    x.append(np.array(img))
+                    y.append(i)
+            except OSError:
+                pass
+            if j % 1000 == 0:
+                print('.', end='')
+        print()
+    return np.array(labels), np.array(x), np.array(y)
+
+
+def remove_duplicates(img_train, labels_train, img_test):
+    img_new, labels_new = [], []
+    test_set = {e.tostring() for e in img_test}
+    for i, (x, y) in enumerate(zip(img_train, labels_train)):
+        if x.tostring() not in test_set:
+            img_new.append(x)
+            labels_new.append(y)
+
+    print(f'Removed {img_train.shape[0] - len(img_new)} duplicated images')
+    return np.array(img_new), np.array(labels_new)
+
+
+def load_not_mnist_data(path='data/not_mnist/', use_cache=True):
+    train_cache_file = Path(path) / 'train.npz'
+    test_cache_file = Path(path) / 'test.npz'
+
+    if train_cache_file.exists() and test_cache_file.exists() and use_cache:
+        print('Loading cached arrays')
+        labels, img_train, labels_train = np.load(train_cache_file).values()
+        labels, img_test, labels_test = np.load(test_cache_file).values()
+
+    else:
+        test_folder = Path(path) / 'small'
+        train_folder = Path(path) / 'large'
+        labels, img_train, labels_train = load_images(test_folder, 10000000)
+        labels, img_test, labels_test = load_images(train_folder, 10000)
+        np.savez(train_cache_file, labels, img_train, labels_train)
+        np.savez(test_cache_file, labels, img_test, labels_test)
+
+    img_train, labels_train = remove_duplicates(img_train, labels_train, img_test)
+    return labels, img_train, labels_train, img_test, labels_test
